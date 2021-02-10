ee9b79a703f6d3dc53594762e2f2f85675c621f6
zhoumingzhu <z@chj.com>
2021-02-10 17:53:01 +0800

    ci

---
+++ server.py
@@ -0,0 +1,41 @@
+import os
+import sys
+
+
+def kill_process_by_name():
+    n = "Flakes/app.py"
+    cmd = "ps -e | grep %s" % n
+    f = os.popen(cmd)
+    txt = f.readlines()
+    if len(txt) == 0:
+        print("no process \"%s\"!!" % n)
+        return
+    else:
+        for line in txt:
+            columns = line.split()
+            pid = columns[0]
+            cmd = "kill -9 %d" % int(pid)
+            rc = os.system(cmd)
+            if rc == 0:
+                print("exec \"%s\" success!!" % cmd)
+            else:
+                print("exec \"%s\" failed!!" % cmd)
+        return
+
+
+def update_codes():
+    print(sys.path[0])
+    os.system("cd " + sys.path[0])
+    lines = os.popen("git pull").readlines()
+    print(lines)
+
+
+def start():
+    lines = os.popen("nohup py app.py > app.log &").readlines()
+    print(lines)
+
+
+if __name__ == "__main__":
+    kill_process_by_name()
+    update_codes()
+    start()
